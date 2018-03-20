af6e2583ffae9c5b58d5d5d883e4bae5ec31245b
AnthonyAbdo1 <abdo.anthony@yahoo.com>
2018-03-20 13:27:10 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace FormFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.fileExtension = new System.Windows.Forms.TextBox();
+            this.searchText = new System.Windows.Forms.TextBox();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.searchTextBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.searchBtn.Location = new System.Drawing.Point(87, 360);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(104, 63);
+            this.searchBtn.TabIndex = 0;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(377, 360);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(106, 63);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // fileExtension
+            // 
+            this.fileExtension.Location = new System.Drawing.Point(87, 51);
+            this.fileExtension.Name = "fileExtension";
+            this.fileExtension.Size = new System.Drawing.Size(68, 22);
+            this.fileExtension.TabIndex = 2;
+            this.fileExtension.TextChanged += new System.EventHandler(this.fileExtension_TextChanged);
+            // 
+            // searchText
+            // 
+            this.searchText.Location = new System.Drawing.Point(87, 90);
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(396, 22);
+            this.searchText.TabIndex = 3;
+            this.searchText.TextChanged += new System.EventHandler(this.searchText_TextChanged);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(87, 12);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(396, 22);
+            this.filePath.TabIndex = 4;
+            this.filePath.TextChanged += new System.EventHandler(this.filePath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "File Ext";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search Txt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Path";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(87, 132);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(396, 193);
+            this.outputBox.TabIndex = 8;
+            this.outputBox.TextChanged += new System.EventHandler(this.outputBox_TextChanged);
+            // 
+            // searchTextBtn
+            // 
+            this.searchTextBtn.Location = new System.Drawing.Point(229, 360);
+            this.searchTextBtn.Name = "searchTextBtn";
+            this.searchTextBtn.Size = new System.Drawing.Size(106, 63);
+            this.searchTextBtn.TabIndex = 9;
+            this.searchTextBtn.Text = "Search Text";
+            this.searchTextBtn.UseVisualStyleBackColor = true;
+            this.searchTextBtn.Click += new System.EventHandler(this.searchTextBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 584);
+            this.Controls.Add(this.searchTextBtn);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.searchText);
+            this.Controls.Add(this.fileExtension);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.TextBox fileExtension;
+        private System.Windows.Forms.TextBox searchText;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button searchTextBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FormFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileExtension_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void filePath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileLogger("File location method was utilized at ", DateTime.Now);
+                string myFilePath = filePath.Text;
+                string myFileExt = fileExtension.Text;
+
+                var i = 0;
+                for (i = 0; i < myFilePath.Length;)
+                {
+
+                    string[] myFiles = Directory.GetFiles(myFilePath, "*" + myFileExt);
+
+                    foreach (string file in myFiles)
+                    {
+                        outputBox.AppendText(file + Environment.NewLine);
+
+                    }
+
+                    break;
+                }
+            }
+
+            catch (FileNotFoundException) { FileLogger("File Not Found Error Thrown at", DateTime.Now); }
+            finally { }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            try {
+                FileLogger("Form fields where cleared at ", DateTime.Now);
+                Action<Control.ControlCollection> func = null;
+
+                func = (controls) =>
+                {
+                    foreach (Control control in controls)
+                        if (control is TextBox)
+                            (control as TextBox).Clear();
+                        else
+                            func(control.Controls);
+                };
+
+                func(Controls);
+            }
+
+            catch (FileNotFoundException) { FileLogger("File Not Found Error Thrown at", DateTime.Now); }
+            finally { }
+        }
+        private void searchTextBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileLogger("Search string method was utilized at ", DateTime.Now);
+                var userFilePathInput = filePath.Text; // text box value for file path
+                var userExtensionInput = fileExtension.Text; // combo box value for extension
+
+                // getting files per user input as arguements
+                var files = Directory.GetFiles(userFilePathInput, "*" + userExtensionInput);
+
+                foreach (var file in files)
+                {
+                    var result = searchFile(file.ToString(), searchText.Text);
+                    if (result == true)
+                    {
+                        outputBox.Text = file;
+                    }
+
+                    else
+                    {
+                        outputBox.Text = "No files returned";
+                    }
+                }
+            }
+
+            catch (FileNotFoundException) { FileLogger("File Not Found Error Thrown at", DateTime.Now); }
+            finally { }
+        }
+
+        private bool searchFile(string fileName, string searchString)
+        {
+            bool stringFound = false;
+
+            FileStream mystream = new FileStream(fileName, FileMode.Open);
+            StreamReader myreader = new StreamReader(mystream);
+
+            while (!myreader.EndOfStream)
+            {
+                var line = myreader.ReadLine();
+
+                if (line.ToUpper().Contains(searchString.ToUpper()))
+                {
+                    stringFound = true;
+                    break;
+                }
+            }
+           
+            return stringFound;           
+        }
+
+        public void FileLogger(string myLog, DateTime theDateTime)
+        {
+            FileStream lFile = new FileStream("log.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter logStreamWriter = new StreamWriter(lFile);
+            logStreamWriter.WriteLine(myLog + theDateTime);
+            logStreamWriter.Close();
+        }   
+    }
+}
